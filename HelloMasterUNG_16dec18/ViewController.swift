cfe6cfe68efd04c4dd9370e5ae7cedd14210df1f
masterUNG <phrombutr@gmail.com>
2018-12-16 10:21:03 +0700

    Start App

---
+++ HelloMasterUNG_16dec18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMasterUNG_16dec18
+//
+//  Created by MasterUNG on 16/12/2561 BE.
+//  Copyright © 2561 MasterUNG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }   // Main Function
+
+
+}   // Main Class
+
